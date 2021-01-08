8bd56d22aac33d3d75ab8d26f266d4542dbb32d9
alf <alf@alf.alf>
2021-01-08 22:29:10 +0700

    DQN pytorch example

--- 01_CartPole-reinforcement-learning/Cartpole_DQN_torch.py
+++ 01_CartPole-reinforcement-learning/Cartpole_DQN_torch.py
@@ -1,37 +1,24 @@
-# Tutorial by www.pylessons.com
-# Tutorial written for - Tensorflow 1.15, Keras 2.2.4
+# Tutorial by www.pylessons.com implementation for pytorch
 
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 import random
 import gym
 import numpy as np
 from collections import deque
-from keras.models import Model, load_model
-from keras.layers import Input, Dense
-from keras.optimizers import Adam, RMSprop
+from torch import nn, optim, zeros, from_numpy, amax, argmax
+from torch import load as load_model
+from torch import save as save_model
+#from sys import exit
 
-
-def OurModel(input_shape, action_space):
-    X_input = Input(input_shape)
-
-    # 'Dense' is the basic form of a neural network layer
-    # Input Layer of state size(4) and Hidden Layer with 512 nodes
-    X = Dense(512, input_shape=input_shape, activation="relu", kernel_initializer='he_uniform')(X_input)
-
-    # Hidden layer with 256 nodes
-    X = Dense(256, activation="relu", kernel_initializer='he_uniform')(X)
-    
-    # Hidden layer with 64 nodes
-    X = Dense(64, activation="relu", kernel_initializer='he_uniform')(X)
-
-    # Output Layer with # of actions: 2 nodes (left, right)
-    X = Dense(action_space, activation="linear", kernel_initializer='he_uniform')(X)
-
-    model = Model(inputs = X_input, outputs = X, name='CartPole DQN model')
-    model.compile(loss="mse", optimizer=RMSprop(lr=0.00025, rho=0.95, epsilon=0.01), metrics=["accuracy"])
-
-    model.summary()
+def getModel(input_shape, action_space):
+    model = nn.Sequential(
+          nn.Linear(input_shape, 512, True),
+          nn.ReLU(),
+          nn.Linear(512, 256, True),
+          nn.ReLU(),
+          nn.Linear(256, 64, True),
+          nn.ReLU(),
+          nn.Linear(64, action_space, True),
+        )
     return model
 
 class DQNAgent:
@@ -40,9 +27,9 @@ class DQNAgent:
         # by default, CartPole-v1 has max episode steps = 500
         self.state_size = self.env.observation_space.shape[0]
         self.action_size = self.env.action_space.n
-        self.EPISODES = 1000
+        self.EPISODES = 2000
         self.memory = deque(maxlen=2000)
-        
+
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.001
@@ -51,7 +38,16 @@ class DQNAgent:
         self.train_start = 1000
 
         # create main model
-        self.model = OurModel(input_shape=(self.state_size,), action_space = self.action_size)
+        self.model = getModel(input_shape=self.state_size, action_space = self.action_size)
+        self.optimizer = optim.RMSprop(self.model.parameters(), lr=0.00025, alpha=0.95, eps=0.01)
+        self.criterion = nn.MSELoss()
+
+    def load(self, name):
+        self.model = load_model(name)
+
+    def save(self, name):
+        print("Model saved!")
+        save_model(self.model, name)
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
@@ -63,31 +59,34 @@ class DQNAgent:
         if np.random.random() <= self.epsilon:
             return random.randrange(self.action_size)
         else:
-            return np.argmax(self.model.predict(state))
+            return argmax(self.model(state)).item()
 
     def replay(self):
         if len(self.memory) < self.train_start:
             return
         # Randomly sample minibatch from the memory
-        minibatch = random.sample(self.memory, min(len(self.memory), self.batch_size))
+        batch_size = min(len(self.memory), self.batch_size)
+        minibatch = random.sample(self.memory, batch_size)
 
-        state = np.zeros((self.batch_size, self.state_size))
-        next_state = np.zeros((self.batch_size, self.state_size))
+        state = zeros((batch_size, self.state_size))
+        next_state = zeros((batch_size, self.state_size))
         action, reward, done = [], [], []
 
         # do this before prediction
         # for speedup, this could be done on the tensor level
         # but easier to understand using a loop
-        for i in range(self.batch_size):
+        for i in range(batch_size):
             state[i] = minibatch[i][0]
             action.append(minibatch[i][1])
             reward.append(minibatch[i][2])
             next_state[i] = minibatch[i][3]
             done.append(minibatch[i][4])
 
+        predicted = self.model(state)
+
         # do batch prediction to save speed
-        target = self.model.predict(state)
-        target_next = self.model.predict(next_state)
+        target = self.model(state)
+        target_next = self.model(next_state)
 
         for i in range(self.batch_size):
             # correction on the Q value for the action used
@@ -98,22 +97,20 @@ class DQNAgent:
                 # DQN chooses the max Q value among next actions
                 # selection and evaluation of action is on the target Q Network
                 # Q_max = max_a' Q_target(s', a')
-                target[i][action[i]] = reward[i] + self.gamma * (np.amax(target_next[i]))
+                target[i][action[i]] = reward[i] + self.gamma * (amax(target_next[i]))
 
         # Train the Neural Network with batches
-        self.model.fit(state, target, batch_size=self.batch_size, verbose=0)
+        self.optimizer.zero_grad()
+        loss = self.criterion(predicted, target)
+        loss.backward()
+        self.optimizer.step()
 
-
-    def load(self, name):
-        self.model = load_model(name)
-
-    def save(self, name):
-        self.model.save(name)
-            
     def run(self):
+        self.model.train()
         for e in range(self.EPISODES):
             state = self.env.reset()
             state = np.reshape(state, [1, self.state_size])
+            state = from_numpy(state).float()
             done = False
             i = 0
             while not done:
@@ -121,6 +118,7 @@ class DQNAgent:
                 action = self.act(state)
                 next_state, reward, done, _ = self.env.step(action)
                 next_state = np.reshape(next_state, [1, self.state_size])
+                next_state = from_numpy(next_state).float()
                 if not done or i == self.env._max_episode_steps-1:
                     reward = reward
                 else:
@@ -128,7 +126,7 @@ class DQNAgent:
                 self.remember(state, action, reward, next_state, done)
                 state = next_state
                 i += 1
-                if done:                   
+                if done:
                     print("episode: {}/{}, score: {}, e: {:.2}".format(e, self.EPISODES, i, self.epsilon))
                     if i == 500:
                         print("Saving trained model as cartpole-dqn.h5")
@@ -137,20 +135,23 @@ class DQNAgent:
                 self.replay()
 
     def test(self):
+        self.model.eval()
         self.load("cartpole-dqn.h5")
         for e in range(self.EPISODES):
             state = self.env.reset()
             state = np.reshape(state, [1, self.state_size])
+            state = from_numpy(state).float()
             done = False
             i = 0
             while not done:
                 self.env.render()
-                action = np.argmax(self.model.predict(state))
+                action = argmax(self.model(state)).item()
                 next_state, reward, done, _ = self.env.step(action)
                 state = np.reshape(next_state, [1, self.state_size])
+                state = from_numpy(state).float()
                 i += 1
                 if done:
-                    print("episode: {}/{}, score: {}".format(e, self.EPISODES, i))
+                    print(f"episode: {e}/{self.EPISODES}, score: {i}")
                     break
 
 if __name__ == "__main__":
